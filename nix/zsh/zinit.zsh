00acf7aa1e4f2e425c8ed7690a2bebe1c6a3d61e
V01d42 <89440437+V01d42@users.noreply.github.com>
2026-01-16 07:30:51 +0000

    fix: rm old plugin

--- nix/zsh/zinit.zsh
+++ nix/zsh/zinit.zsh
@@ -1,17 +1,8 @@
-# oh-my-zsh configuration
-# (Named zinit.zsh for compatibility with home.nix, but uses oh-my-zsh)
-
-# Path to oh-my-zsh installation
-export ZSH="$HOME/.oh-my-zsh"
-
 # Theme (visual appearance handled by p10k, this is fallback)
 ZSH_THEME="agnoster"
 
 # Plugins
 plugins=(git)
-
-# Load oh-my-zsh
-source $ZSH/oh-my-zsh.sh
 
 # direnv hook (if not already loaded by home-manager)
 if [[ -z "$DIRENV_DIR" ]] && command -v direnv &> /dev/null; then
